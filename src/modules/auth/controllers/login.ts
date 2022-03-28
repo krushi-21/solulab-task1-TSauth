6c543eb0a8c0ab36656d042a675474388fba8d7d
krushi21 <krushi.thakkar111@gmail.com>
2022-03-28 12:33:24 +0530

    joi validation

--- src/modules/auth/controllers/login.ts
+++ src/modules/auth/controllers/login.ts
@@ -1,14 +1,15 @@
 import { createToken } from '../../../utils/token';
 import { Request, Response } from 'express';
 import User from '../../../models/user';
+import { authSchema } from '../../../helpers/validationSchema';
 
 export default async function Login(
   req: Request,
   res: Response
 ): Promise<Response | void> {
-  const { email, password } = req.body;
+  const result = await authSchema.validateAsync(req.body);
   //check if user exist
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email: result.email });
   if (!user) {
     return res.status(404).end({
       status: 'fail',
@@ -16,7 +17,7 @@ export default async function Login(
     });
   }
   //check password is matching
-  if (await user.isValidPassword(password)) {
+  if (await user.isValidPassword(result.password)) {
     //assign new JWT token
     const accessToken = createToken(user._id);
     return res.status(200).json({
--- src/modules/auth/controllers/register.ts
+++ src/modules/auth/controllers/register.ts
@@ -1,26 +1,30 @@
 import { Request, Response, NextFunction } from 'express';
 import User from '../../../models/user';
 import { createToken } from '../../../utils/token';
+import { authSchema } from '../../../helpers/validationSchema';
 
 export default async function Register(
   req: Request,
   res: Response
 ): Promise<Response | void> {
   //get user data
-  const { email, password, role } = req.body;
+
+  const { role } = req.body;
+  const result: any = authSchema.validate(req.body.email, req.body.password);
+
   //check user is already exist or not
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email: result.email });
   if (user) {
     return res.status(409).json({
-      status: 'fail',
+      status: 409,
       message: 'User already register with this email',
     });
   }
 
   //save user in Database
   const newUser = await User.create({
-    email,
-    password,
+    email: result.email,
+    password: result.password,
     role,
   });
   //send error if user is not created
